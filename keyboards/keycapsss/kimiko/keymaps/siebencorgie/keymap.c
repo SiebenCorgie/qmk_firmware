e3b3e94dc3d01885ae8b0628376062a841658e1d
Tendsin Mende <tendsin@protonmail.com>
2022-06-22 17:27:07 +0200

    move to hardcoded layer 4

--- keyboards/keycapsss/kimiko/keymaps/siebencorgie/keymap.c
+++ keyboards/keycapsss/kimiko/keymaps/siebencorgie/keymap.c
@@ -46,12 +46,12 @@
 
 enum layers {
     _QWERTY,
-    _LOWER,
+    _L4, //hardcoded layer 4
     _RAISE,
     _ADJUST,
 };
 
-#define RAISE MO(_RAISE)
+#define L4 MO(_L4)
 #define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|   `   |    | togVol|------+------+------+------+------+-----------|
  * |  LCTRL  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift/Enter|
  * `-------------------------------------------|       /     \      \-----------------------------------------------'
- *                 | LCTRL| LGUI | LALT | CAPS | Space /       \Enter \ | BCKSLASH |BackSP| RGUI | RALT |
+ *                 | LCTRL| LGUI | LALT | CAPS | Space /       \Enter \ |LAYER4|BackSP| RGUI | RALT |
  *                 `----------------------------------'       '------------------------------------'
  */
 
@@ -74,29 +74,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  AU_TOG,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-                      KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS, KC_SPC,   KC_ENT,   KC_RALT,   KC_BSPC, KC_RGUI, KC_RALT
+    KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_KB_MUTE,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+                      KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS, KC_SPC,  KC_ENT,    L4,   KC_BSPC, KC_RGUI, KC_RALT
 ),
 /* LOWER
  * ,-------------------------------------------.                    ,-----------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |--------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |        |  1   |  2   |  3   |  4   |  5   |                    |  6   |  7   |  8   |  9   |   0  | F12  |
+ * |        | pgup | l_del|  up  | r_del|pgdow |                    |      |  7   |  8   |  9   |      | F12  |
  * |--------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `    |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   ~  |
+ * |        |ln_sta| left | down | right|ln_end|-------.    ,-------|      |   4  |   5  |   6  |      |      |
  * |--------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |        |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   \  |
+ * |        | esc  |      |insert| enter|      |-------|    |-------|      |   1  |   2  |   3  |      |      |
  * `-------------------------------------------|       /     \      \-----------------------------------------------'
- *                 | LCTRL| LGUI | LALT |LOWER| Space /       \Enter \  |RAISE |BackSP| RGUI | RALT |
+ *                 |      |      |      |      |      /       \      \  |      |      |      |      |
  *                 `----------------------------------'       '------------------------------------'
  */
 
-[_LOWER] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
-    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-    _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS,
-                      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+[_L4] = LAYOUT(
+
+    
+    _______, KC_F1,      KC_F2,     KC_F3,   KC_F4,    KC_F5,                        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,  KC_F11,
+    _______, KC_PAGE_UP, KC_BSPC,   KC_UP,   KC_DEL,   KC_PAGE_DOWN,               _______, KC_7,  KC_8,  KC_9,  KC_0,    KC_F12,
+    _______, _______,   KC_LEFT,   KC_DOWN, KC_RIGHT, KC_END,                     _______, KC_4,  KC_5,  KC_6,  _______, _______,
+    _______, KC_ESC,     _______,   KC_INS,  KC_ENTER, _______,  _______, _______, _______, KC_1,  KC_2,  KC_3,  _______, _______,
+                         _______,   _______, _______,  _______,  _______, _______, _______, _______, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -325,7 +327,7 @@ void render_layer_state(void) {
         0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
     if(layer_state_is(_ADJUST)) {
         oled_write_P(adjust_layer, false);
-    } else if(layer_state_is(_LOWER)) {
+    } else if(layer_state_is(_L4)) {
         oled_write_P(lower_layer, false);
     } else if(layer_state_is(_RAISE)) {
         oled_write_P(raise_layer, false);
@@ -376,7 +378,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         switch (get_highest_layer(layer_state)) {
             // If the Default (QWERTY) layer is active
             case _QWERTY:
-                // Arrow Up/Down
+                // Mouse wheel Up/Down
                 if (clockwise) {
                     tap_code(KC_WH_U);
                 } else {
@@ -385,7 +387,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 break;
 
             // If the RAISE layer is active
-            case _LOWER:
+            case _L4:
                 // Switch browser tabs
                 if (clockwise) {
                     tap_code16(LCTL(KC_TAB));
@@ -409,8 +411,8 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     // Encoder on slave side
     else if (index == 1) {
         switch (get_highest_layer(layer_state)) {
-            // If the Default (QWERTY) layer is active
-            case _LOWER:
+            // If the L4 layer is active
+            case _L4:
                 // Scroll by Word
                 if (clockwise) {
                     tap_code16(LCTL(KC_RGHT));
@@ -419,7 +421,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 }
                 break;
 
-            // If the LOWER layer is active
+            // If the QWERTY layer is active
             case _QWERTY:
                 // Volume up/down
                 if (clockwise) {
