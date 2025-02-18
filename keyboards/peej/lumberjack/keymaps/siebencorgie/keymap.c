a43f2bf417bfc26b11a76523811afa0d290b777b
Sieben <tendsin@protonmail.com>
2025-02-18 14:03:30 +0100

    fix encoder/ non-encoder config

--- keyboards/peej/lumberjack/keymaps/siebencorgie/keymap.c
+++ keyboards/peej/lumberjack/keymaps/siebencorgie/keymap.c
@@ -43,11 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------'           `-----------------------------------------'
      */
     [_QWERTY] = LAYOUT_ortho_5x12(
-        KC_ESC,  KC_1, KC_2, KC_3,    KC_4,    KC_5,   KC_6,   KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,
-        KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,    KC_T,   KC_Y,   KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC,
-        KC_CAPS, KC_A, KC_S, KC_D,    KC_F,    KC_G,   KC_H,   KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z, KC_X, KC_C,    KC_V,    KC_B,   KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC ,
-        KC_LCTL, ____, ____, KC_LGUI, KC_LALT, KC_SPC, KC_ENT, LF,   KC_BSPC, _______, ______, _______),
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7, KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_ESC ,
+        KC_LCTL, _______, _______, KC_LGUI, KC_LALT, KC_SPC, KC_ENT, LF,   KC_BSPC, _______, _______, _______),
 
     /* HardCoded level 4
      * ,------------------------------------------. ,-----------------------------------------.
@@ -65,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LEVEL4] = LAYOUT_ortho_5x12(
 
         _______, KC_F1,      KC_F2,   KC_F3,   KC_F4,    KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, KC_PAGE_UP, KC_BSPC, KC_UP,   KC_DEL,   KC_PAGE_DOWN, _____,   KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
-        _______, _______,    KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,       _______, KC_4,    KC_5,    KC_6,    ______,  KC_VOLU,
-        _______, _______,    _______, _______, _______,  _______,      _______, KC_1,    KC_2,    KC_3,    ______,  KC_VOLD,
-        _______, _______,    _______, _______, _______,  _______,      _______, _______, _____,   _____,   KC_MPLY, KC_MUTE)
+        _______, KC_PAGE_UP, KC_BSPC, KC_UP,   KC_DEL,   KC_PAGE_DOWN, _______, KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
+        _______, _______,    KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,       _______, KC_4,    KC_5,    KC_6,    _______, KC_VOLU,
+        _______, _______,    _______, _______, _______,  _______,      _______, KC_1,    KC_2,    KC_3,    _______, KC_VOLD,
+        _______, _______,    _______, _______, _______,  _______,      _______, _______, _______, _______, KC_MPLY, KC_MUTE)
 
 };
 
